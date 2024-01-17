6499bb238698f70ba87f510f252c05f3e5c953fa
Gareth King <sluglit@gmail.com>
2024-01-17 23:05:47 +0200

    devcontainers

--- .devcontainer/postCreateCommand-cpu.sh
+++ .devcontainer/postCreateCommand-cpu.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+export POETRY_VIRTUALENVS_IN_PROJECT=true
+export PYTHONUNBUFFERED=1
+export PORT=8080
+
+PATH="/root/.local/bin:$PATH"
+PATH=".venv/bin/:$PATH"
+
 pip install --upgrade pip
 
 # Install poetry
 pip install pipx
 python3 -m pipx ensurepath
 pipx install poetry
-PATH="/root/.local/bin:$PATH"
-PATH=".venv/bin/:$PATH"
 
-export POETRY_VIRTUALENVS_IN_PROJECT=true
 poetry install --with local
 poetry install --with ui
-
 poetry run python scripts/setup
